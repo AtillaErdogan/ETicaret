7ec896967cf67e68c546e4f0772c1cc0cf00bf98
AtillaErdogan <mr.erdgn1999@gmail.com>
2025-11-29 23:48:23 +0300

    29.11.2025

---
+++ ETicaretAPI/Infrastructure/ETicaretAPI.Persistence/ServiceRegistration.cs
@@ -0,0 +1,20 @@
+﻿using ETicaretAPI.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Persistence
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistenceServices(this IServiceCollection services)
+        {
+            //IOC Container'a Persistence katmanındaki servislerin eklenmesi işlemleri burada yapılacak
+            services.AddDbContext<ETicaretAPIDbContext>(options => options.UseNpgsql(Configuration.ConnectionString));
+        }
+    }
+}
